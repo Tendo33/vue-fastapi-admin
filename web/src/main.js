82a62b00338b69e202f32a4ffd5d668766a63a55
Tendo33 <sjf1998112@gmail.com>
2025-03-28 16:09:27 +0800

    tiny fix

--- web/src/main.js
+++ web/src/main.js
@@ -7,7 +7,7 @@ import { createApp } from 'vue'
 import { setupRouter } from '@/router'
 import { setupStore } from '@/store'
 import App from './App.vue'
-import { setupDirectives } from './directives'
+import { setupDirectives } from '@/directives'
 import { useResize } from '@/utils'
 import i18n from '~/i18n'
 
